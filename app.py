f4bc2d89f008639f55bde990f0df1d21927382f0
opheliah95 <opheliah95@gmail.com>
2022-01-09 22:02:59 +0000

    feat: initial request to get capital information

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello world from flask"
 
+
 @app.route("/capital/<name>")
 def get_capital(name):
-    return f'finding country related to {name}'
+    url = f'https://restcountries.com/v3.1/capital/{name}'
+    response = requests.get(url)
 
-if __name__=="__main__":
+    if response.status_code == 200:
+        print(f'data found for url: {url}')
+        res_json = response.json()
+        return f'{res_json}'
+    elif response.status_code == 400:
+        return f'{url} is not found'
+    else:
+        return f'unknown error'
+    
+
+
+if __name__ == "__main__":
     app.run(debug=True)
