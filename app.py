12888ea35719a9fe742e5e9f3989f7661997f755
opheliah95 <opheliah95@gmail.com>
2022-01-09 21:22:36 +0000

    feat: added simple routing

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello world from flask"
+
+@app.route("/capital/<name>")
+def get_capital(name):
+    return f'finding country related to {name}'
+
+if __name__=="__main__":
+    app.run(debug=True)
