e035854ec70b616c584f7a628faa2da62437b660
opheliah95 <opheliah95@gmail.com>
2022-01-09 22:12:02 +0000

    feat: initial list of objects

--- app.py
+++ app.py
@@ -16,8 +16,16 @@ def get_capital(name):
 
     if response.status_code == 200:
         print(f'data found for url: {url}')
+        result = {}
         res_json = response.json()
-        return f'{res_json}'
+
+        # a list of country information needed
+        result["country_name"] = res_json[0]["name"]["common"]
+        result["capital"] = res_json[0]["capital"]
+        result["flag"] = res_json[0]["flags"]["png"]
+        result["lang"] = res_json[0]["languages"]
+
+        return f'{result}'
     elif response.status_code == 400:
         return f'{url} is not found'
     else:
